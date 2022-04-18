6e025bbd2d050296c0f0734e649312b845127ccd
sehdez <sergio.hdez.cuevas@gmail.com>
2022-04-18 18:17:21 -0500

    Inicio del proyectos, módulos y angular material agregados

---
+++ src/app/cafeteria/cafeteria.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CafeteriaRoutingModule } from './cafeteria-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    CafeteriaRoutingModule
+  ]
+})
+export class CafeteriaModule { }
